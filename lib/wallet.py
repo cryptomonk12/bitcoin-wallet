spesmilo/electrum

commit 21673c95f4b9546e115fa8a128b0c6bb8c74d4b0

Fix wallet write to support windows

lib/wallet.py
@@ -135,13 +135,16 @@ class WalletStorage(object):
 
     def write(self):
         assert not threading.currentThread().isDaemon()
+        temp_path = self.path + '.tmp'
         s = json.dumps(self.data, indent=4, sort_keys=True)
-        with open(self.path + '.tmp', "w") as f:
+        with open(temp_path, "w") as f:
             f.write(s)
-        os.rename(self.path + '.tmp', self.path)
+        os.remove(self.path)
+        os.rename(temp_path, self.path)
         if 'ANDROID_DATA' not in os.environ:
             import stat
             os.chmod(self.path,stat.S_IREAD | stat.S_IWRITE)
+
 
 
 class Abstract_Wallet(object):

cryptomonk12/bitcoin-wallet

commit 5a7152bed6e16f70b8c582a1d84c17d851186017

fix bug introduced in 21673c95f4b9546e115fa8a128b0c6bb8c74d4b0

lib/wallet.py
@@ -139,8 +139,12 @@ class WalletStorage(object):
         s = json.dumps(self.data, indent=4, sort_keys=True)
         with open(temp_path, "w") as f:
             f.write(s)
-        os.remove(self.path)
-        os.rename(temp_path, self.path)
+        # perform atomic write on POSIX systems
+        try:
+            os.rename(temp_path, self.path)
+        except:
+            os.remove(self.path)
+            os.rename(temp_path, self.path)
         if 'ANDROID_DATA' not in os.environ:
             import stat
             os.chmod(self.path,stat.S_IREAD | stat.S_IWRITE)