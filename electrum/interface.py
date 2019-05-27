spesmilo/electrum

commit 6ec1578a90916436b3bfabe90cafd9bfc804a332

follow-up prev

electrum/interface.py
@@ -167,6 +167,15 @@ class RequestTimedOut(GracefulDisconnect):
 
 class ErrorParsingSSLCert(Exception): pass
 class ErrorGettingSSLCertFromServer(Exception): pass
+class ConnectError(Exception): pass
+
+
+class _Connector(aiorpcx.Connector):
+    async def create_connection(self):
+        try:
+            return await super().create_connection()
+        except OSError as e:
+            raise ConnectError(e)
 
 
 def deserialize_server(server_str: str) -> Tuple[str, str, str]:
@@ -333,8 +342,7 @@ class Interface(Logger):
             return
         try:
             await self.open_session(ssl_context)
-        except (asyncio.CancelledError, ConnectionError, socket.gaierror, aiorpcx.socks.SOCKSError) as e:
-            # note: catching OSError would be too broad here... don't want to catch file system exceptions
+        except (asyncio.CancelledError, ConnectError, aiorpcx.socks.SOCKSError) as e:
             self.logger.info(f'disconnecting due to: {repr(e)}')
             return
 
@@ -419,9 +427,9 @@ class Interface(Logger):
         return self.network.default_server == self.server
 
     async def open_session(self, sslc, exit_early=False):
-        async with aiorpcx.Connector(NotificationSession,
-                                     host=self.host, port=self.port,
-                                     ssl=sslc, proxy=self.proxy) as session:
+        async with _Connector(NotificationSession,
+                              host=self.host, port=self.port,
+                              ssl=sslc, proxy=self.proxy) as session:
             self.session = session  # type: NotificationSession
             self.session.interface = self
             self.session.set_default_timeout(self.network.get_network_timeout_seconds(NetworkTimeout.Generic))

cryptomonk12/bitcoin-wallet

commit 2c9fdb8b6f1e785b87c070ba77243b4a00764d70

interface: fix connecting to new servers using self-signed certs
got broken in 6ec1578a90916436b3bfabe90cafd9bfc804a332

electrum/interface.py
@@ -177,7 +177,8 @@ class _Connector(aiorpcx.Connector):
         try:
             return await super().create_connection()
         except OSError as e:
-            raise ConnectError(e)
+            # note: using "from e" here will set __cause__ of ConnectError
+            raise ConnectError(e) from e
 
 
 def deserialize_server(server_str: str) -> Tuple[str, str, str]:
@@ -254,11 +255,11 @@ class Interface(Logger):
         """
         try:
             await self.open_session(ca_ssl_context, exit_early=True)
-        except ssl.SSLError as e:
-            if e.reason == 'CERTIFICATE_VERIFY_FAILED':
+        except ConnectError as e:
+            cause = e.__cause__
+            if isinstance(cause, ssl.SSLError) and cause.reason == 'CERTIFICATE_VERIFY_FAILED':
                 # failures due to self-signed certs are normal
                 return False
-            # e.g. too weak crypto
             raise
         return True
 